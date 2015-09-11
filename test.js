88f2f7d7f630fa0195638464931c5f23f8d46760
Vladimir Agafonkin <agafonkin@gmail.com>
2015-09-11 12:12:48 +0300

    more test coverage

--- test.js
+++ test.js
@@ -4,8 +4,26 @@ var test = require('tap').test;
 var clip = require('./');
 
 test('clips line', function (t) {
-    var result = clip([[-10, 10], [10, 10], [10, -10]], [0, 0, 20, 20]);
+    var result = clip([
+        [-10, 10], [10, 10], [10, -10], [20, -10], [20, 10], [40, 10],
+        [40, 20], [20, 20], [20, 40], [10, 40], [10, 20], [5, 20], [-10, 20]],
+        [0, 0, 30, 30]);
 
-    t.same(result, [[[0, 10], [10, 10], [10, 0]]]);
+    t.same(result, [
+        [[0, 10], [10, 10], [10, 0]],
+        [[20, 0], [20, 10], [30, 10]],
+        [[30, 20], [20, 20], [20, 30]],
+        [[10, 30], [10, 20], [5, 20], [0, 20]]
+    ]);
+    t.end();
+});
+
+test('appends result if passed third argument', function (t) {
+    var arr = [];
+    var result = clip([[-10, 10], [30, 10]], [0, 0, 20, 20], arr);
+
+    t.same(result, [[[0, 10], [20, 10]]]);
+    t.equal(result, arr);
+
     t.end();
 });
